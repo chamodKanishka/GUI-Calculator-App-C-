afc29e5a603cb5618e8df0246cce5cb3a96e2e43
ChamodKanishka <chamodkanishka77@gmail.com>
2019-11-12 12:11:11 +0530

    create & test with basic functions

---
+++ calculator.h
@@ -0,0 +1,31 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class Calculator;
+}
+
+class Calculator : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Calculator(QWidget *parent = nullptr);
+    ~Calculator();
+
+private:
+    Ui::Calculator *ui;
+    QString currentFile = "";
+
+private slots:
+    void NumPressed();
+    void MathButtonPressed();
+    void EqualButtonPressed();
+    void ChangeNumberSign();
+    void ClearPressed();
+
+};
+
+#endif // CALCULATOR_H
